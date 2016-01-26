e17347c47f9f14f3ed7956e3d664025ba94c8bb1
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2016-01-26 13:21:25 +0200

    NON-REDISTRIBUTABLE! Use libdram blob to initialize H6 DRAM controller
    In order to compile this code, the 'libdram' blob needs to be
    placed into the top level directory in the U-Boot source tree
    (as libdram-h6).
    Version V2_5 and V2_7 from some github H6 BSP repos didn't work for me
    like this, but V2_2 from an early BSP (H6-BSP-1.0.tgz from the Pine64
    H64 wiki page) did.
    
    ============
    PLEASE NOTE: Linking proprietary code with the GPL licensed
    U-Boot sources creates non-redistributable binaries. The GPL license
    requires that full source code needs to be provided together with
    the binaries. It means that if YOU try to create, let's say, a
    binary SD card image with this code, and *distribute* it to other
    people, then these other people have the right to request full
    source code from *YOU* (including the 'libdram' sources too).
    ============
    
    [Andre: - adapt to latest U-Boot and the 32-bit split
    	- add DRAM paramater structure
    	- adapt to H6
    ]

--- arch/arm/mach-sunxi/dram_libdram.c
+++ arch/arm/mach-sunxi/dram_libdram.c
@@ -9,7 +9,69 @@
 #include <asm/arch/dram.h>
 #include <linux/kconfig.h>
 
-unsigned long sunxi_dram_init(void)
+/* Some callback functions, needed for the libdram blob */
+
+void __usdelay(unsigned long us)
+{
+	udelay(us);
+}
+
+int set_ddr_voltage(int set_vol)
 {
 	return 0;
+}
+
+int axp_set_aldo3(int voltage, int enable)
+{
+	return 0;
+}
+
+/*
+ * The init_DRAM function in libdram expects a data structure with the
+ * DRAM parameters. From previous source code releases and by comparing
+ * the parameters these field names have been derived:
+ */
+struct libdram_para {
+	u32 dram_clk;
+	u32 dram_type;
+	u32 dram_zq;
+	u32 dram_odt_en;
+	u32 dram_para1;
+	u32 dram_para2;
+	u32 dram_mr0;
+	u32 dram_mr1;
+	u32 dram_mr2;
+	u32 dram_mr3;
+	u32 dram_mr4;
+	u32 dram_mr5;
+	u32 dram_mr6;
+	u32 dram_tpr0;
+	u32 dram_tpr1;
+	u32 dram_tpr2;
+	u32 dram_tpr3;
+	u32 dram_tpr4;
+	u32 dram_tpr5;
+	u32 dram_tpr6;
+	u32 dram_tpr7;
+	u32 dram_tpr8;
+	u32 dram_tpr9;
+	u32 dram_tpr10;
+	u32 dram_tpr11;
+	u32 dram_tpr12;
+	u32 dram_tpr13;
+	u32 dram_tpr14;
+	u32 dram_tpr15;
+	u32 dram_tpr16;
+	u32 dram_tpr17;
+	u32 dram_tpr18;
+} dram_para = { 0 };
+
+/* Entry point to the libdram blob */
+unsigned long init_DRAM(u32, void *);
+
+unsigned long sunxi_dram_init(void)
+{
+	init_DRAM(0, &dram_para.dram_clk);
+
+	return get_ram_size((long *)PHYS_SDRAM_0, PHYS_SDRAM_0_SIZE);
 }
