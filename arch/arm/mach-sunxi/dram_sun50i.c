5df9ebe2aa01ca014963d456afdf1c36342cd304
Siarhei Siamashka <siarhei.siamashka@gmail.com>
2016-01-26 13:21:25 +0200

    NON-REDISTRIBUTABLE! Use libdram blob to initialize A64 DRAM controller
    In order to compile this code, the 'libdram' blob needs to be
    placed into the top level directory in the U-Boot source tree.
    
    The file in question can be found in the "lichee_linux_310.tar.gz"
    tarball (linked from the pine64 wiki) at the following path:
    "lichee/brandy/u-boot-2014.07/arch/arm/cpu/armv7/sun50iw1p1/dram"
    $ sha256sum libdram
    e23855effc870b194ab0e9623760f1f184f0129f644e0df639329ec2dc838055 libdram
    
    Please note that linking proprietary code with the GPL licensed
    U-Boot sources creates non-redistributable binaries. The GPL license
    requires that full source code needs to be provided together with
    the binaries. It means that if you try to create, let's say, a
    binary SD card image with this code, and *distribute* it to other
    people, then these other people have the right to request full
    source code from *you* (including the 'libdram' sources too).
    
    [Andre: adapting to upstream U-Boot and the 32-bit port]

--- arch/arm/mach-sunxi/dram_sun50i.c
+++ arch/arm/mach-sunxi/dram_sun50i.c
@@ -9,7 +9,60 @@
 #include <asm/arch/dram.h>
 #include <linux/kconfig.h>
 
+/* Some callback functions, needed for the libdram blob */
+
+void __usdelay(unsigned long us)
+{
+	udelay(us);
+}
+
+int set_ddr_voltage(int set_vol)
+{
+	/* DCDC5 is already set as 1.5V by default */
+	return 0;
+}
+
+/* A piece of data, extracted from the Pine64 Android image */
+u32 dram_para[] = {
+    0x000002A0,
+    0x00000003,
+    0x003B3BBB,
+    0x00000001,
+    0x10E410E4,
+    0x00001000,
+    0x00001840,
+    0x00000040,
+    0x00000018,
+    0x00000002,
+    0x004A2195,
+    0x02424190,
+    0x0008B060,
+    0x04B005DC,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00008808,
+    0x20250000,
+    0x00000000,
+    0x04000800,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000
+};
+
+/* Entry point to the libdram blob */
+unsigned long init_DRAM(u32, void *);
+
 unsigned long sunxi_dram_init(void)
 {
-	return 0;
+	init_DRAM(0, &dram_para[0]);
+	return get_ram_size((long *)PHYS_SDRAM_0, PHYS_SDRAM_0_SIZE);
 }
