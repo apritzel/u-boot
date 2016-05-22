34902fe453388d2b067051e6ab416bc3b937fc76
Andre Przywara <andre.przywara@arm.com>
2016-05-23 00:48:27 +0100

    sunxi: A64: do an RMR switch if started in AArch32 mode
    When we start execution with the 32-bit SPL part, both the FEL code
    and the SPL image entry will enter the real U-Boot still in 32-bit
    mode.
    By using a "magic" instruction, which happens to be an almost-NOP on
    AArch64 and a branch on AArch32, we differentiate between being
    entered in 64-bit or 32-bit mode.
    For 64-bit we proceed with the branch to reset, but in 32-bit mode
    we trigger an RMR write to bring the core into AArch64/EL3 and re-enter
    U-Boot at CONFIG_SYS_TEXT_BASE.
    This allows a 32-bit SPL to eventually start a 64-bit U-Boot.
    Adds a menu option to select this alternate BOOT0 boot code.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- arch/arm/include/asm/arch-sunxi/boot0.h
+++ arch/arm/include/asm/arch-sunxi/boot0.h
@@ -4,11 +4,26 @@
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
-#ifndef __BOOT0_H
-#define __BOOT0_H
-
-/* reserve space for BOOT0 header information */
-#define ARM_SOC_BOOT0_HOOK	\
+#ifdef CONFIG_ARM_BOOT_HOOK_BOOT0
+	b	reset
 	.space	1532
-
-#endif /* __BOOT0_H */
+#elif defined(CONFIG_ARM_BOOT_HOOK_RMR)
+	tst     x0, x0                  // this is "b #0x84" in ARM
+	b       reset
+	.space  0x7c
+	.word   0xe3a01617              // mov  r1, #0x1700000
+	.word   0xe38110a0              // orr  r1, r1, #0xa0
+	.word   0xe59f0020              // ldr  r0, [pc, #32]
+	.word   0xe5810000              // str  r0, [r1]
+	.word   0xf57ff04f              // dsb  sy
+	.word   0xf57ff06f              // isb  sy
+	.word   0xee1c0f50              // mrc  15, 0, r0, cr12, cr0, {2}
+	.word   0xe3800003              // orr  r0, r0, #3
+	.word   0xee0c0f50              // mcr  15, 0, r0, cr12, cr0, {2}
+	.word   0xf57ff06f              // isb  sy
+	.word   0xe320f003              // wfi
+	.word   0xeafffffd              // b    @wfi
+	.word   CONFIG_SYS_TEXT_BASE
+#else
+	b	reset
+#endif
