19bad4b1813ef914208ec94a49451fa70e8926fb
Andre Przywara <andre.przywara@arm.com>
2017-06-12 09:49:56 +0100

    arm: nexell: add UART support
    The Nexell S5P6818 SoC uses a UART very similar to those used in the
    Samsung S5P SoCs.
    Enable the driver in the config and add the necessary glue headers
    and clock functions to make the S5P driver happy.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

---
+++ arch/arm/include/asm/arch-nexell/clk.h
@@ -0,0 +1,13 @@
+/*
+ * (C) Copyright 2017 ARM Ltd.
+ * Andre Przywara <andre.przywara@arm.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ASM_ARCH_CLK_H_
+#define __ASM_ARCH_CLK_H_
+
+unsigned long get_uart_clk(int dev_index);
+
+#endif
--- arch/arm/mach-nexell/board.c
+++ arch/arm/mach-nexell/board.c
@@ -5,8 +5,13 @@
  */
 
 #include <common.h>
+#include <asm/io.h>
 
 DECLARE_GLOBAL_DATA_PTR;
+
+#define NEXELL_PLLSETREG0	0xc0010008UL
+
+#define OSC_FREQ 24000000
 
 /* TODO: dummy implementation for now, add proper reset code */
 void reset_cpu(ulong addr)
@@ -33,6 +38,51 @@ int dram_init(void)
 ulong get_tbclk(void)
 {
 	return CONFIG_SYS_HZ;
+}
+
+static unsigned long get_pll_freq(int pll_index)
+{
+	uint32_t reg;
+	unsigned int pdiv, mdiv, sdiv, plloutdiv;
+	unsigned int multfreq;
+
+	if (pll_index < 0 || pll_index > 3)
+		return 0;
+
+	reg = readl(NEXELL_PLLSETREG0 + pll_index * 4);
+	sdiv = reg & 0xff;
+	mdiv = (reg >> 8) & 0x3ff;
+	pdiv = (reg >> 18) & 0x3f;
+	plloutdiv = ((reg >> 24) & 0xf) + 1;
+
+	multfreq = (OSC_FREQ / 1000) * mdiv;
+	return (1000 * (multfreq / (pdiv * 2 * sdiv))) / plloutdiv;
+}
+
+static unsigned long get_level1_clk_freq(uintptr_t base_addr)
+{
+	uint32_t reg;
+	unsigned int pll_index, div;
+
+	reg = readl(base_addr + 0x4);
+	pll_index = (reg >> 2) & 0x7;
+	if (pll_index > 3)
+		return -1UL;
+
+	div = ((reg >> 5) & 0xff) + 1;
+
+	return get_pll_freq(pll_index) / div;
+}
+
+unsigned long get_uart_clk(int dev_index)
+{
+	uintptr_t clock_ofs[6] = {0xc00a9000, 0xc00a8000, 0xc00aa000,
+				  0xc00ab000, 0xc006e000, 0xc0084000};
+
+	if (dev_index < 0 || dev_index > 5)
+		return 0;
+
+	return get_level1_clk_freq(clock_ofs[dev_index]);
 }
 
 int board_init(void)
