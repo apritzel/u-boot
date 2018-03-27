5fb2c92428769ccebe1b9372a4a95c5a64617be4
Andre Przywara <andre.przywara@arm.com>
2018-03-27 01:56:23 +0100

    sunxi: clk: A64: implement UART clocks
    Extend the pristine sunxi DM clock driver to drive the UART clocks.
    This includes functions to determine the current clock rate for the baud
    base clock, needed by the serial driver.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- drivers/clk/sunxi/clk_a64.c
+++ drivers/clk/sunxi/clk_a64.c
@@ -12,16 +12,69 @@
 #include <asm/arch/clock.h>
 #include <dt-bindings/clock/sun50i-a64-ccu.h>
 
+#define OSC_FREQ 24000000
+#define OSC_FREQ_ULL 24000000ULL
+
 struct a64_clk_priv {
 	void *base;
 };
 
+static unsigned int get_pll_rate(void *base)
+{
+	uint32_t reg = readl(base);
+	int n, k, m, p;
+
+	if (!(reg & BIT(31)))
+		return 0;
+
+	n = ((reg >> 8) & 0x1f) + 1;
+	k = ((reg >> 4) & 0x03) + 1;
+	m = ((reg >> 0) & 0x03) + 1;
+	p = ((reg >> 16) & 0x3) + 1;
+
+	return OSC_FREQ_ULL * n * k / (m * p);
+}
+
+
+static unsigned int get_apb2_rate(void *base)
+{
+	uint32_t reg = readl(base + 0x58), ret;
+	unsigned int n, m;
+
+	switch ((reg >> 24) & 0x3) {
+	case 0:
+		ret = 32768;
+		break;
+	case 1:
+		ret = OSC_FREQ;
+		break;
+	case 2:
+	case 3:
+		ret = get_pll_rate(base + 0x28);	/* PLL_PERIPH0 */
+		break;
+	}
+	n = 1U << ((reg >> 16) & 0x3);
+	m = ((reg >> 0) & 0x1f) + 1;
+
+	return ret / (n * m);
+}
+
 static ulong a64_clk_get_rate(struct clk *clk)
 {
-	debug("%s(#%ld)\n", __FUNCTION__, clk->id);
+	struct a64_clk_priv *priv = dev_get_priv(clk->dev);
 
-	debug("  unhandled\n");
-	return -EINVAL;
+	debug("%s(#%ld)\n", __func__, clk->id);
+	switch(clk->id) {
+	case CLK_BUS_UART0:
+	case CLK_BUS_UART1:
+	case CLK_BUS_UART2:
+	case CLK_BUS_UART3:
+	case CLK_BUS_UART4:
+		return get_apb2_rate(priv->base);
+	default:
+		debug("  unhandled\n");
+		return -EINVAL;
+	}
 }
 
 static ulong a64_clk_set_rate(struct clk *clk, ulong rate)
@@ -34,10 +87,23 @@ static ulong a64_clk_set_rate(struct clk *clk, ulong rate)
 
 static int a64_clk_enable(struct clk *clk)
 {
-	debug("%s(#%ld)\n", __FUNCTION__, clk->id);
+	struct a64_clk_priv *priv = dev_get_priv(clk->dev);
+	void *clk_base;
 
-	debug("  unhandled\n");
-	return -EINVAL;
+	debug("%s(#%ld)\n", __func__, clk->id);
+	switch(clk->id) {
+	case CLK_BUS_UART0:
+	case CLK_BUS_UART1:
+	case CLK_BUS_UART2:
+	case CLK_BUS_UART3:
+	case CLK_BUS_UART4:
+		setbits_le32(priv->base + 0x6c,
+			     BIT(16 + (clk->id - CLK_BUS_UART0)));
+		return 0;
+	default:
+		debug("  unhandled\n");
+		return -ENODEV;
+	}
 }
 
 static struct clk_ops a64_clk_ops = {
