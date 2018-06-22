5cd4d87c010dafc6bc6a5165fba737a9d729a25a
Andre Przywara <andre.przywara@arm.com>
2018-06-22 14:19:11 +0100

    sunxi: print_cpuinfo(): switch to runtime detection
    Now that we allow to initialise the UART according to the SoC-ID
    detected at runtime, we should make use of that and print the initial
    greeting also depending on the actual SoC.
    
    Change the #ifdef cascade to a switch-case construct, driven by the
    SoC-ID. Work out the generation and SoC name accordingly.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- arch/arm/mach-sunxi/cpu_info.c
+++ arch/arm/mach-sunxi/cpu_info.c
@@ -54,54 +54,49 @@ unsigned int sunxi_read_socid(void)
 #ifdef CONFIG_DISPLAY_CPUINFO
 int print_cpuinfo(void)
 {
-#ifdef CONFIG_MACH_SUN4I
-	puts("CPU:   Allwinner A10 (SUN4I)\n");
-#elif defined CONFIG_MACH_SUN5I
-	u32 val = readl(SUNXI_SID_BASE + 0x08);
-	switch ((val >> 12) & 0xf) {
-	case 0: puts("CPU:   Allwinner A12 (SUN5I)\n"); break;
-	case 3: puts("CPU:   Allwinner A13 (SUN5I)\n"); break;
-	case 7: puts("CPU:   Allwinner A10s (SUN5I)\n"); break;
-	default: puts("CPU:   Allwinner A1X (SUN5I)\n");
-	}
-#elif defined CONFIG_MACH_SUN6I
-	switch (sunxi_get_ss_bonding_id()) {
-	case SUNXI_SS_BOND_ID_A31:
-		puts("CPU:   Allwinner A31 (SUN6I)\n");
+	uint socid = sunxi_read_socid();
+	char *socname;
+	int generation = 0;
+	u32 val;
+
+	switch (socid) {
+	case SOCID_A10: socname = "A10"; generation = 4; break;
+	case SOCID_A13:
+		generation = 5;
+		val = readl(SUNXI_SID_BASE + 0x08);
+		switch ((val >> 12) & 0xf) {
+		case 0: socname = "A12"; break;
+		case 3: socname = "A13"; break;
+		case 7: socname = "A10s"; break;
+		default: socname = "A1X"; break;
+		}
 		break;
-	case SUNXI_SS_BOND_ID_A31S:
-		puts("CPU:   Allwinner A31s (SUN6I)\n");
+	case SOCID_A31:
+		generation = 6;
+		switch (sunxi_get_ss_bonding_id()) {
+		case SUNXI_SS_BOND_ID_A31: socname = "A31"; break;
+		case SUNXI_SS_BOND_ID_A31S: socname = "A31s"; break;
+		default: socname = "A31?"; break;
+		}
 		break;
+	case SOCID_A20: generation = 7; socname = "A20"; break;
+	case SOCID_A23: generation = 8; socname = "A23"; break;
+	case SOCID_A33: generation = 8; socname = "A33"; break;
+	case SOCID_A83T: generation = 8; socname = "A83T"; break;
+	case SOCID_H3: generation = 8; socname = "H3"; break;
+	case SOCID_R40: generation = 8; socname = "R40"; break;
+	case SOCID_V3S: generation = 8; socname = "V3s"; break;
+	case SOCID_A80: generation = 9; socname = "A80"; break;
+	case SOCID_A64: generation = 50; socname = "A64"; break;
+	case SOCID_H5: generation = 50; socname = "H5"; break;
+	case SOCID_H6: generation = 50; socname = "H6"; break;		 
 	default:
-		printf("CPU:   Allwinner A31? (SUN6I, id: %d)\n",
-		       sunxi_get_ss_bonding_id());
+		puts("CPU: unknown SUNXI family\n");
+		return 0;
 	}
-#elif defined CONFIG_MACH_SUN7I
-	puts("CPU:   Allwinner A20 (SUN7I)\n");
-#elif defined CONFIG_MACH_SUN8I_A23
-	printf("CPU:   Allwinner A23 (SUN8I %04x)\n", sunxi_read_socid());
-#elif defined CONFIG_MACH_SUN8I_A33
-	printf("CPU:   Allwinner A33 (SUN8I %04x)\n", sunxi_read_socid());
-#elif defined CONFIG_MACH_SUN8I_A83T
-	printf("CPU:   Allwinner A83T (SUN8I %04x)\n", sunxi_read_socid());
-#elif defined CONFIG_MACH_SUN8I_H3
-	printf("CPU:   Allwinner H3 (SUN8I %04x)\n", sunxi_read_socid());
-#elif defined CONFIG_MACH_SUN8I_R40
-	printf("CPU:   Allwinner R40 (SUN8I %04x)\n", sunxi_read_socid());
-#elif defined CONFIG_MACH_SUN8I_V3S
-	printf("CPU:   Allwinner V3s (SUN8I %04x)\n", sunxi_read_socid());
-#elif defined CONFIG_MACH_SUN9I
-	puts("CPU:   Allwinner A80 (SUN9I)\n");
-#elif defined CONFIG_MACH_SUN50I
-	puts("CPU:   Allwinner A64 (SUN50I)\n");
-#elif defined CONFIG_MACH_SUN50I_H5
-	puts("CPU:   Allwinner H5 (SUN50I)\n");
-#elif defined CONFIG_MACH_SUN50I_H6
-	puts("CPU:   Allwinner H6 (SUN50I)\n");
-#else
-#warning Please update cpu_info.c with correct CPU information
-	puts("CPU:   SUNXI Family\n");
-#endif
+	printf("CPU:   Allwinner %s (SUN%dI %04x)\n", socname, generation,
+	       socid);
+
 	return 0;
 }
 #endif
