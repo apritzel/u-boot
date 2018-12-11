458f795e538ba3b58200c3f5f70e02e4ef4fefde
Andre Przywara <andre.przywara@arm.com>
2018-12-11 21:50:14 +0000

    sunxi: H6: Use libdram for Eachlink H6 Mini SPL
    Those are the parameters read from the image shipped with the
    Eachlink H6 Mini TV box. The Android is rooted, so the boot0 image can
    be dumped from sector 16 of the eMMC on the Android shell prompt.
    This also adds a eachlink_h6_mini_libdram_spl_defconfig to trigger
    the SPL build with libdram.

--- arch/arm/mach-sunxi/dram_libdram.c
+++ arch/arm/mach-sunxi/dram_libdram.c
@@ -64,7 +64,36 @@ struct libdram_para {
 	u32 dram_tpr16;
 	u32 dram_tpr17;
 	u32 dram_tpr18;
-} dram_para = { 0 };
+} dram_para = {
+	/* The values are from the boot0 dump from the H6 Mini eMMC image. */
+	.dram_clk = 840,
+	.dram_type = 3,
+	.dram_zq = 0x3b3bfb,
+	.dram_odt_en = 1,
+	.dram_para1 = 0x30fb,
+	.dram_para2 = 0x08000000,
+	.dram_mr0 = 0x1e14,
+	.dram_mr1 = 0x40,
+	.dram_mr2 = 0x20,
+	.dram_mr3 = 0x0,
+	.dram_mr4 = 0x0,
+	.dram_mr5 = 0x400,
+	.dram_mr6 = 0x848,
+	.dram_tpr0 = 0x124aa9d7,
+	.dram_tpr1 = 0x124239d0,
+	.dram_tpr2 = 0x89306612,
+	.dram_tpr3 = 0x78787896,
+	.dram_tpr4 = 0x0,
+	.dram_tpr5 = 0x0,
+	.dram_tpr6 = 0x09090900,
+	.dram_tpr7 = 0x1e08a1e0,
+	.dram_tpr8 = 0x0,
+	.dram_tpr9 = 0x0,
+	.dram_tpr10 = 0x80000000,
+	.dram_tpr11 = 0x0,
+	.dram_tpr12 = 0x1212,
+	.dram_tpr13 = 0x2001,
+};
 
 /* Entry point to the libdram blob */
 unsigned long init_DRAM(u32, void *);
