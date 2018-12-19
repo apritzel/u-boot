9e184a82455625f11de760dac6b36210f1af57b3
Andre Przywara <andre.przywara@arm.com>
2018-12-19 16:42:58 +0000

    sunxi: Add more SOCID definitions
    So far the SOCID_ values are only used for a subset of the supported
    parts.
    
    Add the remaining values so that we can start doing autodetection.
    
    This also moves the definition before the inclusion of cpu_sun<x>i.h, so
    that we can use them in there as well.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- arch/arm/include/asm/arch-sunxi/cpu.h
+++ arch/arm/include/asm/arch-sunxi/cpu.h
@@ -6,6 +6,21 @@
 #ifndef _SUNXI_CPU_H
 #define _SUNXI_CPU_H
 
+#define SOCID_A10	0x1623
+#define SOCID_A13	0x1625
+#define SOCID_A31	0x1633
+#define SOCID_A80	0x1639
+#define SOCID_A23	0x1650
+#define SOCID_A20	0x1651
+#define SOCID_A33	0x1667
+#define SOCID_A83T	0x1673
+#define SOCID_H3	0x1680
+#define SOCID_V3S	0x1681
+#define SOCID_A64	0x1689
+#define SOCID_R40	0x1701
+#define SOCID_H5	0x1718
+#define SOCID_H6	0x1728
+
 #if defined(CONFIG_MACH_SUN9I)
 #include <asm/arch/cpu_sun9i.h>
 #elif defined(CONFIG_MACH_SUN50I_H6)
@@ -13,11 +28,5 @@
 #else
 #include <asm/arch/cpu_sun4i.h>
 #endif
-
-#define SOCID_A64	0x1689
-#define SOCID_H3	0x1680
-#define SOCID_V3S	0x1681
-#define SOCID_H5	0x1718
-#define SOCID_R40	0x1701
 
 #endif /* _SUNXI_CPU_H */
