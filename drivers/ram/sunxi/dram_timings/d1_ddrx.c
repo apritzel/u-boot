142c76adf42dc44267677e8e2f8f7ac10027b9d4
Andre Przywara <andre.przywara@arm.com>
2022-12-30 12:26:49 +0000

    timings: re-indent switch/case, remove brackets

--- drivers/ram/sunxi/dram_timings/d1_ddrx.c
+++ drivers/ram/sunxi/dram_timings/d1_ddrx.c
@@ -171,170 +171,162 @@ void mctl_set_timing_params(dram_para_t *para)
 	}
 
 	switch (para->dram_type) {
-		case 2: // DDR2
-		{
-			trasmax = CONFIG_DRAM_CLK / 30;
-			if (CONFIG_DRAM_CLK < 409) {
-				tcl		   = 3;
-				t_rdata_en = 1;
-				mr0		   = 0x06a3;
-			} else {
-				t_rdata_en = 2;
-				tcl		   = 4;
-				mr0		   = 0x0e73;
-			}
-			tmrd	   = 2;
-			twtp	   = twr + 5;
+	case 2: // DDR2
+		trasmax = CONFIG_DRAM_CLK / 30;
+		if (CONFIG_DRAM_CLK < 409) {
+			tcl		   = 3;
+			t_rdata_en = 1;
+			mr0		   = 0x06a3;
+		} else {
+			t_rdata_en = 2;
+			tcl		   = 4;
+			mr0		   = 0x0e73;
+		}
+		tmrd	   = 2;
+		twtp	   = twr + 5;
+		tcksrx	   = 5;
+		tckesr	   = 4;
+		trd2wr	   = 4;
+		tcke	   = 3;
+		tmod	   = 12;
+		wr_latency = 1;
+		mr3		   = 0;
+		mr2		   = 0;
+		tdinit0	   = 200 * CONFIG_DRAM_CLK + 1;
+		tdinit1	   = 100 * CONFIG_DRAM_CLK / 1000 + 1;
+		tdinit2	   = 200 * CONFIG_DRAM_CLK + 1;
+		tdinit3	   = 1 * CONFIG_DRAM_CLK + 1;
+		tmrw	   = 0;
+		twr2rd	   = twtr + 5;
+		tcwl	   = 0;
+		mr1		   = para->dram_mr1;
+		break;
+	case 3: // DDR3
+		trasmax = CONFIG_DRAM_CLK / 30;
+		if (CONFIG_DRAM_CLK <= 800) {
+			mr0		   = 0x1c70;
+			tcl		   = 6;
+			wr_latency = 2;
+			tcwl	   = 4;
+			mr2		   = 24;
+		} else {
+			mr0		   = 0x1e14;
+			tcl		   = 7;
+			wr_latency = 3;
+			tcwl	   = 5;
+			mr2		   = 32;
+		}
+
+		twtp   = tcwl + 2 + twtr; // WL+BL/2+tWTR
+		trd2wr = tcwl + 2 + twr; // WL+BL/2+tWR
+		twr2rd = tcwl + twtr; // WL+tWTR
+
+		tdinit0 = 500 * CONFIG_DRAM_CLK + 1; // 500 us
+		tdinit1 = 360 * CONFIG_DRAM_CLK / 1000 + 1; // 360 ns
+		tdinit2 = 200 * CONFIG_DRAM_CLK + 1; // 200 us
+		tdinit3 = 1 * CONFIG_DRAM_CLK + 1; //   1 us
+
+		if (((para->dram_tpr13 >> 2) & 0x03) == 0x01 || CONFIG_DRAM_CLK < 912) {
+			mr1		   = para->dram_mr1;
+			t_rdata_en = tcwl; // a5 <- a4
 			tcksrx	   = 5;
 			tckesr	   = 4;
-			trd2wr	   = 4;
-			tcke	   = 3;
-			tmod	   = 12;
-			wr_latency = 1;
-			mr3		   = 0;
-			mr2		   = 0;
-			tdinit0	   = 200 * CONFIG_DRAM_CLK + 1;
-			tdinit1	   = 100 * CONFIG_DRAM_CLK / 1000 + 1;
-			tdinit2	   = 200 * CONFIG_DRAM_CLK + 1;
-			tdinit3	   = 1 * CONFIG_DRAM_CLK + 1;
-			tmrw	   = 0;
-			twr2rd	   = twtr + 5;
-			tcwl	   = 0;
+			trd2wr	   = 5;
+		} else {
 			mr1		   = para->dram_mr1;
-			break;
-		}
-		case 3: // DDR3
-		{
-			trasmax = CONFIG_DRAM_CLK / 30;
-			if (CONFIG_DRAM_CLK <= 800) {
-				mr0		   = 0x1c70;
-				tcl		   = 6;
-				wr_latency = 2;
-				tcwl	   = 4;
-				mr2		   = 24;
-			} else {
-				mr0		   = 0x1e14;
-				tcl		   = 7;
-				wr_latency = 3;
-				tcwl	   = 5;
-				mr2		   = 32;
-			}
-
-			twtp   = tcwl + 2 + twtr; // WL+BL/2+tWTR
-			trd2wr = tcwl + 2 + twr; // WL+BL/2+tWR
-			twr2rd = tcwl + twtr; // WL+tWTR
-
-			tdinit0 = 500 * CONFIG_DRAM_CLK + 1; // 500 us
-			tdinit1 = 360 * CONFIG_DRAM_CLK / 1000 + 1; // 360 ns
-			tdinit2 = 200 * CONFIG_DRAM_CLK + 1; // 200 us
-			tdinit3 = 1 * CONFIG_DRAM_CLK + 1; //   1 us
-
-			if (((para->dram_tpr13 >> 2) & 0x03) == 0x01 || CONFIG_DRAM_CLK < 912) {
-				mr1		   = para->dram_mr1;
-				t_rdata_en = tcwl; // a5 <- a4
-				tcksrx	   = 5;
-				tckesr	   = 4;
-				trd2wr	   = 5;
-			} else {
-				mr1		   = para->dram_mr1;
-				t_rdata_en = tcwl; // a5 <- a4
-				tcksrx	   = 5;
-				tckesr	   = 4;
-				trd2wr	   = 6;
-			}
-			tcke = 3; // not in .S ?
-			tmod = 12;
-			tmrd = 4;
-			tmrw = 0;
-			mr3	 = 0;
-			break;
-		}
-		case 6: // LPDDR2
-		{
-			trasmax	   = CONFIG_DRAM_CLK / 60;
-			mr3		   = para->dram_mr3;
-			twtp	   = twr + 5;
-			mr2		   = 6;
-			mr1		   = 5;
+			t_rdata_en = tcwl; // a5 <- a4
 			tcksrx	   = 5;
-			tckesr	   = 5;
-			trd2wr	   = 10;
-			tcke	   = 2;
-			tmod	   = 5;
-			tmrd	   = 5;
-			tmrw	   = 3;
-			tcl		   = 4;
-			wr_latency = 1;
-			t_rdata_en = 1;
-			tdinit0	   = 200 * CONFIG_DRAM_CLK + 1;
-			tdinit1	   = 100 * CONFIG_DRAM_CLK / 1000 + 1;
-			tdinit2	   = 11 * CONFIG_DRAM_CLK + 1;
-			tdinit3	   = 1 * CONFIG_DRAM_CLK + 1;
-			twr2rd	   = twtr + 5;
-			tcwl	   = 2;
-			mr1		   = 195;
-			mr0		   = 0;
-			break;
+			tckesr	   = 4;
+			trd2wr	   = 6;
 		}
+		tcke = 3; // not in .S ?
+		tmod = 12;
+		tmrd = 4;
+		tmrw = 0;
+		mr3	 = 0;
+		break;
+	case 6: // LPDDR2
+		trasmax	   = CONFIG_DRAM_CLK / 60;
+		mr3		   = para->dram_mr3;
+		twtp	   = twr + 5;
+		mr2		   = 6;
+		mr1		   = 5;
+		tcksrx	   = 5;
+		tckesr	   = 5;
+		trd2wr	   = 10;
+		tcke	   = 2;
+		tmod	   = 5;
+		tmrd	   = 5;
+		tmrw	   = 3;
+		tcl		   = 4;
+		wr_latency = 1;
+		t_rdata_en = 1;
+		tdinit0	   = 200 * CONFIG_DRAM_CLK + 1;
+		tdinit1	   = 100 * CONFIG_DRAM_CLK / 1000 + 1;
+		tdinit2	   = 11 * CONFIG_DRAM_CLK + 1;
+		tdinit3	   = 1 * CONFIG_DRAM_CLK + 1;
+		twr2rd	   = twtr + 5;
+		tcwl	   = 2;
+		mr1		   = 195;
+		mr0		   = 0;
+		break;
 
-		case 7: // LPDDR3
-		{
-			trasmax = CONFIG_DRAM_CLK / 60;
-			if (CONFIG_DRAM_CLK < 800) {
-				tcwl	   = 4;
-				wr_latency = 3;
-				t_rdata_en = 6;
-				mr2		   = 12;
-			} else {
-				tcwl	   = 3;
-				tcke	   = 6;
-				wr_latency = 2;
-				t_rdata_en = 5;
-				mr2		   = 10;
-			}
-			twtp	= tcwl + 5;
-			tcl		= 7;
-			mr3		= para->dram_mr3;
-			tcksrx	= 5;
-			tckesr	= 5;
-			trd2wr	= 13;
-			tcke	= 3;
-			tmod	= 12;
-			tdinit0 = 400 * CONFIG_DRAM_CLK + 1;
-			tdinit1 = 500 * CONFIG_DRAM_CLK / 1000 + 1;
-			tdinit2 = 11 * CONFIG_DRAM_CLK + 1;
-			tdinit3 = 1 * CONFIG_DRAM_CLK + 1;
-			tmrd	= 5;
-			tmrw	= 5;
-			twr2rd	= tcwl + twtr + 5;
-			mr1		= 195;
-			mr0		= 0;
-			break;
+	case 7: // LPDDR3
+		trasmax = CONFIG_DRAM_CLK / 60;
+		if (CONFIG_DRAM_CLK < 800) {
+			tcwl	   = 4;
+			wr_latency = 3;
+			t_rdata_en = 6;
+			mr2		   = 12;
+		} else {
+			tcwl	   = 3;
+			tcke	   = 6;
+			wr_latency = 2;
+			t_rdata_en = 5;
+			mr2		   = 10;
 		}
-		default:
-			twr2rd	   = 8; // 48(sp)
-			tcksrx	   = 4; // t1
-			tckesr	   = 3; // t4
-			trd2wr	   = 4; // t6
-			trasmax	   = 27; // t3
-			twtp	   = 12; // s6
-			tcke	   = 2; // s8
-			tmod	   = 6; // t0
-			tmrd	   = 2; // t5
-			tmrw	   = 0; // a1
-			tcwl	   = 3; // a5
-			tcl		   = 3; // a0
-			wr_latency = 1; // a7
-			t_rdata_en = 1; // a4
-			mr3		   = 0; // s0
-			mr2		   = 0; // t2
-			mr1		   = 0; // s1
-			mr0		   = 0; // a3
-			tdinit3	   = 0; // 40(sp)
-			tdinit2	   = 0; // 32(sp)
-			tdinit1	   = 0; // 24(sp)
-			tdinit0	   = 0; // 16(sp)
-			break;
+		twtp	= tcwl + 5;
+		tcl		= 7;
+		mr3		= para->dram_mr3;
+		tcksrx	= 5;
+		tckesr	= 5;
+		trd2wr	= 13;
+		tcke	= 3;
+		tmod	= 12;
+		tdinit0 = 400 * CONFIG_DRAM_CLK + 1;
+		tdinit1 = 500 * CONFIG_DRAM_CLK / 1000 + 1;
+		tdinit2 = 11 * CONFIG_DRAM_CLK + 1;
+		tdinit3 = 1 * CONFIG_DRAM_CLK + 1;
+		tmrd	= 5;
+		tmrw	= 5;
+		twr2rd	= tcwl + twtr + 5;
+		mr1		= 195;
+		mr0		= 0;
+		break;
+	default:
+		twr2rd	   = 8; // 48(sp)
+		tcksrx	   = 4; // t1
+		tckesr	   = 3; // t4
+		trd2wr	   = 4; // t6
+		trasmax	   = 27; // t3
+		twtp	   = 12; // s6
+		tcke	   = 2; // s8
+		tmod	   = 6; // t0
+		tmrd	   = 2; // t5
+		tmrw	   = 0; // a1
+		tcwl	   = 3; // a5
+		tcl		   = 3; // a0
+		wr_latency = 1; // a7
+		t_rdata_en = 1; // a4
+		mr3		   = 0; // s0
+		mr2		   = 0; // t2
+		mr1		   = 0; // s1
+		mr0		   = 0; // a3
+		tdinit3	   = 0; // 40(sp)
+		tdinit2	   = 0; // 32(sp)
+		tdinit1	   = 0; // 24(sp)
+		tdinit0	   = 0; // 16(sp)
+		break;
 	}
 
 	/* Set mode registers */
